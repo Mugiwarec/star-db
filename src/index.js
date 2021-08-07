49c16e7aacedf48aa9bcbbfd2bdd3afabfb8d25c
Vyacheslav <yupigrand@gmail.com>
2021-08-07 21:26:39 +0300

    handled error

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-
 const getResource = async (url) => {
     const res = await fetch(url);
+
+    if (!res.ok) {
+        throw new Error(`Could not fetch ${url}` +
+            `, received ${res.status}`);
+    }
+
     return await res.json();
 }
 
-getResource('https://swapi.dev/api/people/1')
+getResource('https://swapi.dev/api/people/123232')
     .then((body) => {
         console.log(body);
+    })
+    .catch((err) => {
+        console.error('Could not fetch', err);
     });
-
-// fetch('https://swapi.dev/api/people/1')
-//     .then((res) => {
-//         return res.json();
-//     })
-//     .then((body) => {
-//         console.log(body);
-//     });
