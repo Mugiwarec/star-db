439d8e3afb40338290d6b5ad1354fe1d5da17c94
Vyacheslav <yupigrand@gmail.com>
2021-08-07 21:19:37 +0300

    examples fetch work

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 
-console.log('Hello world!');
+const getResource = async (url) => {
+    const res = await fetch(url);
+    return await res.json();
+}
+
+getResource('https://swapi.dev/api/people/1')
+    .then((body) => {
+        console.log(body);
+    });
+
+// fetch('https://swapi.dev/api/people/1')
+//     .then((res) => {
+//         return res.json();
+//     })
+//     .then((body) => {
+//         console.log(body);
+//     });
