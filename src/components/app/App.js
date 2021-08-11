be3feb10c452d82d6cd32c8353ecb497505ccf0f
Vyacheslav <yupigrand@gmail.com>
2021-08-11 16:40:03 +0300

    added onServiceChanged;

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -19,15 +19,25 @@ import './App.css';
 
 export default class App extends Component {
 
-    swapiService = new SwapiService();
-
     state = {
-        showRandomPlanet: true
+        showRandomPlanet: true,
+        swapiService: new DummySwapiService()
     };
 
     componentDidCatch() {
         console.log('componentDidCatch()');
         this.setState({hasError: true});
+    };
+
+    onServiceChange = () => {
+        this.setState(({swapiService}) => {
+            const Service = swapiService instanceof SwapiService ? DummySwapiService : SwapiService;
+            console.log('switched to', Service.name);
+
+            return {
+                swapiService: new Service()
+            };
+        });
     };
 
     toggleRandomPlanet = () => {
@@ -41,41 +51,13 @@ export default class App extends Component {
     render() {
 
         const planet = this.state.showRandomPlanet ?
-            <RandomPlanet/> :
-            null;
-
-        const {getAllPeople, getAllPlanets} = this.swapiService;
-
-        // const personDetails = (
-        //     <ItemDetails
-        //         itemId={11}
-        //         getData={getPerson}
-        //         getImageUrl={getPersonImage}>
-        //
-        //         <Record field="gender" label="Gender"/>
-        //         <Record field="eyeColor" label="Eye Color"/>
-        //
-        //     </ItemDetails>
-        // );
-        //
-        // const starshipDetails = (
-        //     <ItemDetails
-        //         itemId={5}
-        //         getData={getStarship}
-        //         getImageUrl={getStarshipImage}>
-        //
-        //         <Record field="model" label="Model"/>
-        //         <Record field="length" label="Length"/>
-        //         <Record field="costInCredits" label="Cost"/>
-        //
-        //     </ItemDetails>
-        // );
+            <RandomPlanet/> : null;
 
         return (
             <ErrorBoundry>
-                <SwapiServiceProvider value={this.swapiService}>
+                <SwapiServiceProvider value={this.state.swapiService}>
                     <div className="stardb-app">
-                        <Header/>
+                        <Header onServiceChange={this.onServiceChange}/>
                         {planet}
 
                         <PersonDetails itemId={11}/>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,29 +1,32 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 import './Header.css';
 
-export default class Header extends Component {
+const Header = ({onServiceChange}) => {
+    return (
+        <div className="header d-flex">
+            <h3>
+                <a href="#">
+                    Star DB
+                </a>
+            </h3>
+            <ul className="d-flex">
+                <li>
+                    <a href="#">People</a>
+                </li>
+                <li>
+                    <a href="#">Planets</a>
+                </li>
+                <li>
+                    <a href="#">Starships</a>
+                </li>
+            </ul>
 
-    render() {
-        return (
-            <div className="header d-flex">
-                <h3>
-                    <a href="#">
-                        Star DB
-                    </a>
-                </h3>
-                <ul className="d-flex">
-                    <li>
-                        <a href="#">People</a>
-                    </li>
-                    <li>
-                        <a href="#">Planets</a>
-                    </li>
-                    <li>
-                        <a href="#">Starships</a>
-                    </li>
-                </ul>
-            </div>
-        );
-    };
+            <button className="btn btn-primary btn-sm" onClick={onServiceChange}>
+                Change Service
+            </button>
+        </div>
+    );
 };
+
+export default Header;
