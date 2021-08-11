98a1656ac14e27e74e19056f1ad64b7b498a9bfe
Vyacheslav <yupigrand@gmail.com>
2021-08-11 15:56:55 +0300

    mapped props

--- src/components/sw_components/planet_details.js
+++ src/components/sw_components/planet_details.js
@@ -1,29 +1,25 @@
 import React from "react";
 
 import ItemDetails, {Record} from "../item_details";
-import {SwapiServiceConsumer} from "../swapi_service_context";
+import {withSwapiService} from "../hoc_helpers";
 
-const PlanetDetails = ({itemId}) => {
+const PlanetDetails = (props) => {
     return (
-        <SwapiServiceConsumer>
-            {
-                ({getPlanet, getPlanetImage}) => {
-                    return (
-                        <ItemDetails
-                            itemId={itemId}
-                            getData={getPlanet}
-                            getImageUrl={getPlanetImage}>
+        <ItemDetails {...props}>
 
-                            <Record field="population" label="Population"/>
-                            <Record field="rotationPeriod" label="Rotation Period"/>
-                            <Record field="diameter" label="Diameter"/>
+            <Record field="population" label="Population"/>
+            <Record field="rotationPeriod" label="Rotation Period"/>
+            <Record field="diameter" label="Diameter"/>
 
-                        </ItemDetails>
-                    );
-                }
-            }
-        </SwapiServiceConsumer>
+        </ItemDetails>
     );
 };
 
-export default PlanetDetails;
+const mapMethodsToProps = (swapiService) => {
+    return {
+        getData: swapiService.getPlanet,
+        getImageUrl: swapiService.getPlanetImage
+    };
+};
+
+export default withSwapiService(PlanetDetails, mapMethodsToProps);
