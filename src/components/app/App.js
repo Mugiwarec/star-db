4c0dab2b2b92d456904f52fbbbca217539aca595
Vyacheslav <yupigrand@gmail.com>
2021-08-15 14:47:18 +0300

    Опциональные параметры

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -48,7 +48,7 @@ export default class App extends Component {
               <Route path="/"
                      render={() => <h2>Welcome to STARDB</h2>}
                      exact/>
-              <Route path="/people" component={PeoplePage}/>
+              <Route path="/people/:id?" component={PeoplePage}/>
               <Route path="/planets" component={PlanetsPage}/>
               <Route path="/starships" exact component={StarshipsPage}/>
               <Route path="/starships/:id"
