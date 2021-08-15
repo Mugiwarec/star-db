8b61f23bb666d75acc550480522e8f07ea51d2bc
Vyacheslav <yupigrand@gmail.com>
2021-08-15 13:46:33 +0300

    Добавил Routing для PeoplePage, PlanetsPage, StarshipsPage

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,55 +1,58 @@
 import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import Header from '../header';
 import RandomPlanet from '../random_planet';
-import SwapiService from "../../services/SwapiService";
-import DummySwapiService from "../../services/dummy-swapi-service";
-import ErrorBoundry from "../error_boundry";
+import SwapiService from '../../services/SwapiService';
+import DummySwapiService from '../../services/dummy-swapi-service';
+import ErrorBoundry from '../error_boundry';
 import {PeoplePage, PlanetsPage, StarshipsPage} from '../pages';
-import {SwapiServiceProvider} from "../swapi_service_context";
+import {SwapiServiceProvider} from '../swapi_service_context';
 
 import './App.css';
 
 export default class App extends Component {
 
-    state = {
-        swapiService: new SwapiService()
-    };
+  state = {
+    swapiService: new SwapiService(),
+  };
 
-    componentDidCatch() {
-        console.log('componentDidCatch()');
-        this.setState({hasError: true});
-    };
+  componentDidCatch() {
+    console.log('componentDidCatch()');
+    this.setState({hasError: true});
+  };
 
-    onServiceChange = () => {
-        this.setState(({swapiService}) => {
-            const Service = swapiService instanceof SwapiService ? DummySwapiService : SwapiService;
-            console.log('switched to', Service.name);
+  onServiceChange = () => {
+    this.setState(({swapiService}) => {
+      const Service = swapiService instanceof SwapiService
+        ? DummySwapiService
+        : SwapiService;
+      console.log('switched to', Service.name);
 
-            return {
-                swapiService: new Service()
-            };
-        });
-    };
+      return {
+        swapiService: new Service(),
+      };
+    });
+  };
 
-    render() {
-        return (
-            <ErrorBoundry>
-                <SwapiServiceProvider value={this.state.swapiService}>
-                    <div className="stardb-app">
-                        <Header onServiceChange={this.onServiceChange}/>
+  render() {
+    return (
+      <ErrorBoundry>
+        <SwapiServiceProvider value={this.state.swapiService}>
+          <Router>
+            <div className="stardb-app">
+              <Header onServiceChange={this.onServiceChange}/>
+              <RandomPlanet/>
 
-                        <RandomPlanet/>
+              <Route path="/" render={() => <h2>Welcome to STARDB</h2>} exact/>
+              <Route path="/people" component={PeoplePage}/>
+              <Route path="/planets" component={PlanetsPage}/>
+              <Route path="/starships" component={StarshipsPage}/>
 
-                        <PeoplePage/>
-
-                        <PlanetsPage/>
-
-                        <StarshipsPage/>
-
-                    </div>
-                </SwapiServiceProvider>
-            </ErrorBoundry>
-        );
-    };
+            </div>
+          </Router>
+        </SwapiServiceProvider>
+      </ErrorBoundry>
+    );
+  };
 };
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,32 +1,33 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 import './Header.css';
 
 const Header = ({onServiceChange}) => {
-    return (
-        <div className="header d-flex">
-            <h3>
-                <a href="#">
-                    Star DB
-                </a>
-            </h3>
-            <ul className="d-flex">
-                <li>
-                    <a href="#">People</a>
-                </li>
-                <li>
-                    <a href="#">Planets</a>
-                </li>
-                <li>
-                    <a href="#">Starships</a>
-                </li>
-            </ul>
+  return (
+    <div className="header d-flex">
+      <h3>
+        <Link to="/">
+          Star DB
+        </Link>
+      </h3>
+      <ul className="d-flex">
+        <li>
+          <Link to="/people">People</Link>
+        </li>
+        <li>
+          <Link to="/planets">Planets</Link>
+        </li>
+        <li>
+          <Link to="/starships">Starships</Link>
+        </li>
+      </ul>
 
-            <button className="btn btn-primary btn-sm" onClick={onServiceChange}>
-                Change Service
-            </button>
-        </div>
-    );
+      <button className="btn btn-primary btn-sm" onClick={onServiceChange}>
+        Change Service
+      </button>
+    </div>
+  );
 };
 
 export default Header;
